2ca0971edb98a00e620e47866f0bb05ba0b0dc75
alxndro373 <josealex2004@hotmail.com>
2024-05-15 12:16:06 -0600

    Changes in terminal

--- client/src/hooks/useTerminales.js
+++ client/src/hooks/useTerminales.js
@@ -5,22 +5,49 @@ export const useTerminales = () => {
     const [terminales, setTerminales] = useState([])
 
     const fetchTerminales = async () => {
-        const res = await getTerminalesRequest()
-        setTerminales(res.data)
+        try {
+            const res = await getTerminalesRequest()
+            setTerminales(res.data)
+        } catch (error) {
+            console.error("Error al obtener las ciudades")
+        }
     }
     const handleCreateTerminales = async (values) => {
-        await createTerminalesRequest(values)
-        fetchTerminales()
-    }
+        try {
+            if (values.idTerminales || !values.nombre || !values.direccion || !values.latitud || !values.longitud) {
+                alert("Todos los campos son obligatorios")
+                return
+            }
+            await createTerminalesRequest(values)
+            fetchTerminales()
+            alert("Terminal registrada correctamente")
+        } catch (error) {
+            console.error("Error al registrar la terminal", error)
+            alert("Error al registrar la terminal")
+        }
+    } 
     const handleDeleteTerminales = async (id) => {
-        await deleteTerminalesRequest(id)
-        fetchTerminales()
+        try {
+            await deleteTerminalesRequest(id)
+            fetchTerminales()
+            alert("Terminal eliminada correctamente")
+        } catch (error) {
+            console.error("Error al eliminar la terminal", error)
+            alert("Error al eliminar la terminal")
+        }
     }
     const handleUpdateTerminales = async (id,fields) => {
-        const result = await updateTerminalesRequest(id,fields)
-        console.log(result)
-        if(result.status == 200 && result.data.affectedRows > 0) alert("Actualizado Correctamente")
-        else alert("Actualizacion Fallida")
+        try {
+            const result = await updateTerminalesRequest(id,fields)
+            if (result.status == 200 && result.data.affectedRows > 0) {
+                alert("Actualizado Correctamente")  
+            } else{
+                alert("No se pudo actualizar la terminal")
+            }
+        } catch (error) {
+            console.error("Error al actualizar la terminal", error)
+            alert("Error al actualizar la terminal")
+        }
     }
 
     return {
